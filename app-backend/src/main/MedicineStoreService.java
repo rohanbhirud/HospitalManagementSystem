999c615db208f3f9bb37052b6942f2feff9a7a04
shubham198-ai <121715079+shubham198-ai@users.noreply.github.com>
2023-03-04 23:08:55 +0530

    Delete MedicineStoreService.java

--- app-backend/src/main/MedicineStoreService.java
+++
@@ -1,7 +0,0 @@
-package hospital.backend.service;
-
-import hospital.backend.models.MedicineStore;
-
-public interface MedicineStoreService {
-	public MedicineStore loadMedicineStoreByMedicineId(Integer medicineId);
-}
